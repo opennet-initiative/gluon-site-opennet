8935f2defe225b486d3fc3ce7e69d1bcd6adb47d
Christoph Kottke <christoph.kottke@gmx.de>
2025-03-19 21:33:18 +0100

    neustart

--- image-customization.lua
+++ image-customization.lua
@@ -1,27 +1,17 @@
 packages {
-	'iwinfo',
 	'tcpdump'
 }
 
 features {
-	'alfred',
 	'authorized-keys',
-	'autoupdater',
-	'ebtables-filter-multicast',
-	'ebtables-filter-ra-dhcp',
-	'ebtables-limit-arp',
-	'mesh-batman-adv-15',
-	'mesh-vpn-fastd',
-	'respondd',
-	'status-page',
-	'web-advanced',
-	'web-wizard'
+	'client-bridge',	-- warum
+	'mesh-olsrd',
+
+	-- 'mesh-vpn-fastd'
 }
 
 if not device_class('tiny') then
 	features {
-		'wireless-encryption-wpa3',
-		'web-private-wifi',
 		'tls'
 	}
 end
